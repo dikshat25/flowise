8c2ef40f0d7ea052c36e0ff96d9c195aac6dae4c
dikshat25 <161052953+dikshat25@users.noreply.github.com>
2026-09-25 20:59:10 +0530

    Update vite.config.js

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,48 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config()
-
-export default defineConfig(() => {
-    let proxy = undefined
-
-    const serverHost = process.env.HOST ?? 'localhost'
-    const serverPort = process.env.PORT ?? 3000
-
-    if (process.env.NODE_ENV === 'development') {
-        proxy = {
-            '^/api': {
-                target: `http://${serverHost}:${serverPort}`,
-                changeOrigin: true
-            }
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
         }
-    }
-
-    return {
-        plugins: [react()],
-        resolve: {
-            alias: {
-                '@': resolve(__dirname, 'src'),
-                '@ui': resolve(__dirname, 'src/ui'),
-                '@assets': resolve(__dirname, 'src/assets'),
-                '@hooks': resolve(__dirname, 'src/hooks'),
-                '@utils': resolve(__dirname, 'src/utils'),
-                '@ui-components': resolve(__dirname, 'src/ui-component'),
-                '@views': resolve(__dirname, 'src/views'),
-                '@store': resolve(__dirname, 'src/store'),
-                '@routes': resolve(__dirname, 'src/routes')
-            }
-        },
-        build: {
-            outDir: './build'
-        },
-        server: {
-            open: true,
-            proxy,
-            port: Number(process.env.VITE_PORT ?? 8080),
-            host: process.env.VITE_HOST
-        }
+    },
+    server: {
+        port: 8080
+    },
+    build: {
+        outDir: 'build'
     }
 })
